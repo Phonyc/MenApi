15ac7ea694e0b31eecd576bc81798395c791f126
Phonyc <119613433+Phonyc@users.noreply.github.com>
2023-12-03 18:17:27 +0100

    Fix new Ui

--- MenApi.py
+++ MenApi.py
@@ -23,15 +23,15 @@ class MenApi:
 
     def get_page(self, date: datetime.datetime, moment='diners') -> str:
 
-        req = requests.get(self.base_url + urllib.parse.quote(f'{moment}/semaines/{date.year}-{date.month}-{date.day}'))
+        req = requests.get(self.base_url + urllib.parse.quote(f'repas/semaines/{moment}/{date.year}-{date.month}-{date.day}'))
         return req.text
     
     def parse_text(self, content: str, date: datetime.datetime) -> list[str]:
         page_bs4 = BeautifulSoup(content, 'html.parser')
         page_bs4.body.main.section.h2.contents[0].split('du')[1].split('au')
-        dates_semaines = page_bs4.body.main.section.h2.contents[0].split('du')[1].split('au')
+        text_start = page_bs4.body.main.section.h2.text.replace('\n', '').split(' du ')[1].split(' au ')[0].strip().split(' ')
         
-        start_date = datetime.datetime(date.year, int(self.corresp[unidecode.unidecode(dates_semaines[0].split(' ')[2].lower())]), int(dates_semaines[0].split(' ')[1]))
+        start_date = datetime.datetime(date.year, int(self.corresp[unidecode.unidecode(text_start[1].lower())]), int(text_start[0]))
         # end_date = (date.year, int(self.corresp[unidecode.unidecode(dates_semaines[1].split(' ')[2].lower())]), int(dates_semaines[1].split(' ')[1]))
         num_item = (date - start_date).days
         menu = []
@@ -51,11 +51,10 @@ class MenApi:
         return menu
 
     def get_at_date_midi(self, date: datetime.datetime) -> list[str]:
-        return self.parse_text(self.get_page(date, moment='déjeuners'), date)
+        return self.parse_text(self.get_page(date, moment='dejeuners'), date)
     
     def get_at_date_soir(self, date: datetime.datetime) -> list[str]:
         return self.parse_text(self.get_page(date, moment='diners'), date)
 
 # client = MenApi()
-# print(client.get_at_date_midi(datetime.datetime(2023, 11, 10)))
-# print(client.get_at_date_soir(datetime.datetime(2023, 11, 10)))
+# print(client.get_at_date_midi(datetime.datetime(2023, 12, 4)))
